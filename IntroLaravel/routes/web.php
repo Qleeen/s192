c87e254cc68b41798ddaff84274e4a5a16096d49
Baruch O <146269567+Qleeen@users.noreply.github.com>
2024-10-09 18:58:06 -0600

    Practica 4

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,19 +1,30 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+// use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+
+Route::get('/', [controladorVistas::class, 'home'])->name('rutainicio');
+
+Route::get('/formulario', [controladorVistas::class, 'formulario'])->name('rutaform');
+
+Route::get('/clientes', [controladorVistas::class, 'consulta'])->name('rutaclientes');
+
+Route::view('/component', 'componentes')->name('componente');
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 // Ruta de tipo GET
 // Route::get('/', function () {
@@ -21,11 +32,15 @@ Route::middleware('auth')->group(function () {
 // });
 
 // Ruta de tipo vista
-Route::view('/', 'inicio')->name('rutainicio');
 
-Route::view('/formulario', 'formulario')->name('rutaform');
 
-Route::view('/clientes', 'clientes')->name('rutaclientes');
+// Route::view('/', 'inicio')->name('rutainicio');
 
-Route::view('/component', 'componentes')->name('componente');
+// Route::view('/formulario', 'formulario')->name('rutaform');
+
+// Route::view('/clientes', 'clientes')->name('rutaclientes');
+
+// Route::view('/component', 'componentes')->name('componente');
+
+
 require __DIR__.'/auth.php';
