fe7964ff61398f84f5e2fe72333f6ed05655233d
Baruch O <146269567+Qleeen@users.noreply.github.com>
2024-10-31 14:46:25 -0600

    traduccion
    Se arreglo un espacio de mas en la traduccion de una palabra jeje

--- Repaso2/resources/views/registroLibro.blade.php
+++ Repaso2/resources/views/registroLibro.blade.php
@@ -22,37 +22,37 @@
         <div class="mb-3">
             <label for="isbn" class="form-label">ISBN</label>
             <input type="text" class="form-control" name="txtisbn" value="{{ old('txtisbn') }}">
-            <small class="first-italic text-danger">{{ $errors->first('txtisbn') }}</small>
+            <small class="first-italic text-warning">{{ $errors->first('txtisbn') }}</small>
         </div>
         <div class="mb-3">
             <label for="titulo" class="form-label">{{__('Titulo')}}</label>
             <input type="text" class="form-control" name="txttitulo" value="{{ old('txttitulo') }}">
-            <small class="first-italic text-danger">{{ $errors->first('txttitulo') }}</small>
+            <small class="first-italic text-warning">{{ $errors->first('txttitulo') }}</small>
         </div>
         <div class="mb-3">
             <label for="autor" class="form-label">{{__('Autor')}}</label>
             <input type="text" class="form-control" name="txtautor" value="{{ old('txtautor') }}">
-            <small class="first-italic text-danger">{{ $errors->first('txtautor') }}</small>
+            <small class="first-italic text-warning">{{ $errors->first('txtautor') }}</small>
         </div>
         <div class="mb-3">
             <label for="paginas" class="form-label">{{__('Paginas')}}</label>
             <input type="number" class="form-control" name="txtpaginas" value="{{ old('txtpaginas') }}">
-            <small class="first-italic text-danger">{{ $errors->first('txtpaginas') }}</small>
+            <small class="first-italic text-warning">{{ $errors->first('txtpaginas') }}</small>
         </div>
         <div class="mb-3">
             <label for="anio" class="form-label">{{__('Año')}}</label>
             <input type="number" class="form-control" name="txtanio" value="{{ old('txtanio') }}">
-            <small class="first-italic text-danger">{{ $errors->first('txtanio') }}</small>
+            <small class="first-italic text-warning">{{ $errors->first('txtanio') }}</small>
         </div>
         <div class="mb-3">
             <label for="editorial" class="form-label">{{__('Editorial')}}</label>
             <input type="text" class="form-control" name="txteditorial" value="{{ old('txteditorial') }}">
-            <small class="first-italic text-danger">{{ $errors->first('txteditorial') }}</small>
+            <small class="first-italic text-warning">{{ $errors->first('txteditorial') }}</small>
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">{{__('Email')}}</label>
             <input type="email" class="form-control" name="txtemail" value="{{ old('txtemail') }}">
-            <small class="first-italic text-danger">{{ $errors->first('txtemail') }}</small>
+            <small class="first-italic text-warning">{{ $errors->first('txtemail') }}</small>
         </div>
         <button type="submit" class="btn btn-danger">{{__('Registrar Libro')}}</button>
     </form>
